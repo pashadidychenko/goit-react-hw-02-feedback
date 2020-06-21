8ecc40da911ad1d846e76e22d2418176654502dd
pashadidychenko <pasha.didychenko@gmail.com>
2020-06-21 22:38:45 +0300

    Updates

---
+++ precache-manifest.ba3ff2e657900e2956f02126559af8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd048d88fdc01a3f2d0cfa7592896337",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "7d773eded506f589f8a6",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b7ed841a11542028301",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c9919ea1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c9919ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d773eded506f589f8a6",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fcdaf0b7.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
